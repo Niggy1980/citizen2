fd4034194b513eff4a26810d71b9ee36103bf421
Garfield567 <86929560+Garfield567@users.noreply.github.com>
2024-03-10 07:31:19 +0700

    1

--- lib/feature/USER/presentation/Homepage/NewsPage.dart
+++ lib/feature/USER/presentation/Homepage/NewsPage.dart
@@ -5,17 +5,17 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 
 class NewsPage extends StatelessWidget {
 
-   NewsPage({super.key});
+  NewsPage({super.key});
 
   final TextEditingController newPostController = TextEditingController();
 
-   @override
+  @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Center(child:Image(image: AssetImage ('assets/image/citizenicon.png'),width: 50,)),
+        title: Center(child: Image(
+          image: AssetImage('assets/image/citizenicon.png'), width: 50,)),
         backgroundColor: Color.fromRGBO(68, 117, 182, 1.0),
       ),
       drawer: MyDrawer(),
@@ -25,14 +25,22 @@ class NewsPage extends StatelessWidget {
         activeColor: Colors.white,
         tabBackgroundColor: Color.fromRGBO(219, 226, 239, 100),
         padding: EdgeInsets.all(16),
-        tabs:  [
-          GButton(icon: Icons.home, text: 'Home',onPressed:(){ Navigator.pushNamed(context,'/homepage');},) ,
-          GButton (icon: Icons.newspaper, text: 'News',onPressed:(){ Navigator.pushNamed(context,'/newspage');},),
-          GButton(icon: Icons.notifications_active, text: 'Notification',onPressed:(){ Navigator.pushNamed(context,'/notipage');},),
+        tabs: [
+          GButton(icon: Icons.home, text: 'Home', onPressed: () {
+            Navigator.pushNamed(context, '/homepage');
+          },),
+          GButton(icon: Icons.newspaper, text: 'News', onPressed: () {
+            Navigator.pushNamed(context, '/newspage');
+          },),
+          GButton(icon: Icons.notifications_active,
+            text: 'Notification',
+            onPressed: () {
+              Navigator.pushNamed(context, '/notipage');
+            },),
         ],
 
       ),
 
-      );
-    }
+    );
+  }
 }
